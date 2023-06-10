318bd88040223b01ce8d42620cc84f77d924d173
jmjubaer <jmjubaer3927@gmail.com>
2023-06-10 10:56:29 +0600

    make custom hooks for custom axios

---
+++ src/Hooks/useAxiosSecured.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import useAuthContext from './UseAuthContext';
+import { useNavigate } from 'react-router-dom';
+
+const useAxiosSecured = () => {
+    const {logout} = useAuthContext();
+    const navigate = useNavigate();
+    const axiosSecured = axios.create({
+        baseURL: 'http://localhost:5000'
+    })
+    useEffect(()=> {
+        axiosSecured.interceptors.request.use((request) => {
+            const token = localStorage.getItem('music-hub-token');
+            if (token) {
+                request.headers.Authorization = `Bearer ${token}`
+            }
+            return request;
+        })
+        axiosSecured.interceptors.response.use((response) => response,
+        async(error) => {
+            if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+                await logout();
+                navigate('/login')
+            }
+        }
+    )
+    },[]);
+    return[axiosSecured]
+};
+
+export default useAxiosSecured;
